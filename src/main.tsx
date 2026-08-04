bf7505272de02a0defc02c3735d26c47d693e7a1
meguikalan-ai <meguikalan@gmail.com>
2026-08-04 20:33:43 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import AuthGate from './AuthGate'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthGate>
+      <App />
+    </AuthGate>
   </React.StrictMode>,
 )
-
-// Registro do Service Worker (necessário para o app ser instalável como PWA)
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch((err) => {
-      console.error('Falha ao registrar o Service Worker:', err)
-    })
-  })
-}
